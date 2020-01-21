32844da1040f956b581daba13e4a1aa161e8dee4
Wally Jones <wally@wallyjones.com>
2020-01-21 17:12:33 -0500

    Use environment variables for API token in testing

--- pinboard_test.go
+++ pinboard_test.go
@@ -2,12 +2,23 @@ package pinboard
 
 import (
 	"fmt"
+	"os"
 	"reflect"
 	"testing"
 )
 
-// A valid token is needed to make calls to the Pinboard API.
-var token = ""
+var token string
+
+func TestMain(m *testing.M) {
+	tokenEnv, ok := os.LookupEnv("PINBOARD_TOKEN")
+	if !ok {
+		fmt.Println("pinboard token env not set")
+		os.Exit(1)
+	}
+
+	token = tokenEnv
+	os.Exit(m.Run())
+}
 
 func PrintStruct(s interface{}) {
 	structType := reflect.TypeOf(s)
@@ -23,15 +34,15 @@ func PrintStruct(s interface{}) {
 
 func TestAdd(t *testing.T) {
 
-	p := new(Post)
-
-	p.Token = token
-	p.Description = "TESTING PINBOARD CLI CLIENT"
-	p.URL = "https://github.com/imwally/pinboard/testing"
-	p.Tags = "pin pinboard test testing"
-	p.Extended = "This is a test from imwally's golang pinboard package. For more information please refer to the pinned URL."
-	p.Toread = "yes"
-	p.Shared = "yes"
+	p := Post{
+		Token:       token,
+		Description: "TESTING PINBOARD CLI CLIENT",
+		URL:         "https://github.com/imwally/pinboard",
+		Tags:        "pin pinboard test testing",
+		Extended:    "This is a test from imwally's golang pinboard package. For more information please refer to the pinned URL.",
+		Toread:      "yes",
+		Shared:      "yes",
+	}
 
 	t.Log("Adding: ", p.URL)
 	err := p.Add()
@@ -41,10 +52,10 @@ func TestAdd(t *testing.T) {
 }
 
 func TestShow(t *testing.T) {
-	p := new(Post)
-
-	p.Token = token
-	p.Count = 2
+	p := Post{
+		Token: token,
+		Count: 2,
+	}
 
 	recent := p.ShowRecent()
 	for _, r := range recent.Posts {
@@ -54,15 +65,14 @@ func TestShow(t *testing.T) {
 }
 
 func TestDelete(t *testing.T) {
-	p := new(Post)
-
-	p.Token = token
-	p.URL = "https://github.com/imwally/pinboard/testing"
+	p := Post{
+		Token: token,
+		URL:   "https://github.com/imwally/pinboard/testing",
+	}
 
 	t.Log("Deleting: ", p.URL)
 	err := p.Delete()
 	if err != nil {
 		t.Error(err)
 	}
-
 }
